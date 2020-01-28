b3528e98051aee20b1e257728e5643a1f836286a
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-28 14:39:40 -0800

    updating AddFriend.js

--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 //import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-export const AddFriend = ()=>{
-    return (
-        <h1>This is your AddFriend Form:</h1>
-    )
-
-}
+export const AddFriend = () => {
+  return (
+    <div>
+      <h1>This is your AddFriend Form:</h1>
+      <form onSubmit={this.login}>
+        <input
+          type="text"
+          name="username"
+          value={this.state.credentials.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={this.state.credentials.password}
+          onChange={this.handleChange}
+        />
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddFriend;
