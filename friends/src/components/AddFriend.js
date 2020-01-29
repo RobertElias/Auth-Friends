cea1e9125777cdefc17dec55db4b1f66e57c0f86
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-28 16:11:19 -0800

    AddFriends.js completed.

--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -1,24 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 //import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-export const AddFriend = () => {
+export const AddFriend = props => {
+  const [form, setForm] = useState({
+    id: Date.now(),
+    name: "",
+    age: "",
+    email: ""
+  });
+
   return (
     <div>
       <h1>This is your AddFriend Form:</h1>
-      <form onSubmit={this.login}>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          axiosWithAuth()
+            .post("/api/friends", form)
+            .then(res => {
+              console.log(res);
+              props.history.push("./friendsList");
+            })
+            .catch(error => console.log(error));
+        }}
+      >
+        <input
+        //   type="text"
+          name="name"
+          placeholder="Name"
+          value={form.name}
+          onChange={e => {
+            setForm({ ...form, [e.target.name]: e.target.value });
+          }}
+        />
+        <input
+        //   type="text"
+          type="text"
+          name="age"
+          placeholder="Age"
+          value={form.age}
+          onChange={e => {
+            setForm({ ...form, [e.target.name]: e.target.value });
+          }}
+        />
         <input
           type="text"
-          name="username"
-          value={this.state.credentials.username}
-          onChange={this.handleChange}
+          name="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={e => {
+            setForm({ ...form, [e.target.name]: e.target.value });
+          }}
         />
-        <input
-          type="password"
-          name="password"
-          value={this.state.credentials.password}
-          onChange={this.handleChange}
-        />
-        <button type="submit">Log in</button>
+        <button type="submit">Add A Friend</button>
       </form>
     </div>
   );
